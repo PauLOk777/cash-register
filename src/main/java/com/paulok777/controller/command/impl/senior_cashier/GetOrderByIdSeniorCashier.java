3da5015d95e88abe4ee6d81f33344b48ee2d559f
PauLOk777 <trocjuk2000@gmail.com>
2020-10-12 15:07:01 +0300

    Added commands for senior cashier

---
+++ src/main/java/com/paulok777/controller/command/impl/senior_cashier/GetOrderByIdSeniorCashier.java
@@ -0,0 +1,12 @@
+package com.paulok777.controller.command.impl.senior_cashier;
+
+import com.paulok777.controller.command.Command;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class GetOrderByIdSeniorCashier implements Command {
+    @Override
+    public String execute(HttpServletRequest request) {
+        return null;
+    }
+}
