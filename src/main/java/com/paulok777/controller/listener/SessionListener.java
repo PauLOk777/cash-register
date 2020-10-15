3887bcc8fa9155c7ea2451d113d0f9712cf0cf23
PauLOk777 <trocjuk2000@gmail.com>
2020-10-15 10:59:41 +0300

    Added session listener, to controll destroy proccess of session

---
+++ src/main/java/com/paulok777/controller/listener/SessionListener.java
@@ -0,0 +1,23 @@
+package com.paulok777.controller.listener;
+
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+import java.util.HashSet;
+
+public class SessionListener implements HttpSessionListener {
+    @Override
+    public void sessionCreated(HttpSessionEvent se) {
+
+    }
+
+    @Override
+    public void sessionDestroyed(HttpSessionEvent se) {
+        @SuppressWarnings("unchecked")
+        HashSet<String> loggedUsers = (HashSet<String>) se
+                .getSession().getServletContext()
+                .getAttribute("loggedUsers");
+        String userName = (String) se.getSession().getAttribute("username");
+        loggedUsers.remove(userName);
+        se.getSession().setAttribute("loggedUsers", loggedUsers);
+    }
+}
